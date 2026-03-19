c1024a6b5c3350cc50e006b28888226a1333fc54
Chebt <Chebetdaniel590@gmail.com>
2026-03-19 19:26:59 +0300

    Final push for the css and images changes

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="login-box">
